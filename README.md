6eb407ad7e3b2de7bcf40767ee307b96702d88d9
David Yeung <quxbaz@gmail.com>
2020-11-11 00:28:07 -0700

    Template files.

---
+++ README.md
@@ -0,0 +1,11 @@
+# quick-state
+Lorem ipsum.
+
+[License](#license).
+
+## Table of contents
+1. [foo](#foo)
+2. [bar](#bar)
+
+## License
+[MIT](https://www.mit.edu/~amini/LICENSE.md)
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ["test"],
+  // collectCoverageFrom: ['src/*.{ts,tsx}'],
+  // coverageDirectory: path.resolve(process.cwd(), 'coverage'),
+}
