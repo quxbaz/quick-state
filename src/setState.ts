71f6be6d5308d2274c650344d24f36ce0a0d93a9
David Yeung <quxbaz@gmail.com>
2020-11-11 19:27:14 -0700

    Update comments, names.

--- src/setState.ts
+++ src/setState.ts
@@ -5,40 +5,42 @@ const isObject = (value: any) => (
 )
 
 /*
+  Updates an object's value at a path and returns a new object.
+
   The most important state/store related function. It merges
-  transforms into the store state at the given `path`.
+  transform objects into the store state at the given `path`.
 
   It works by recursively traversing into `object` by reading each
   element of the `path` array.
 
   When the function has drilled down to the full `path`, it applies
-  `state` to the value at that point.
+  `props` to the value at that point.
 
-  If `state` is an object, it will merge with the existing
-  state. Otherwise, if `state` is a primitive or array, it will
+  If `props` is an object, it will merge with the existing
+  state. Otherwise, if `props` is a primitive or array, it will
   replace the existing state.
 */
-function update (object: PlainObject, path: Key[], state: any): PlainObject {
+function update (object: PlainObject, path: Key[], props: any): PlainObject {
 
   if (path.length === 0) {
     // Warning: You are dynamically creating a top-level namespace. Normally you
     // would define this in your initial state object.
-    return {...object, ...state}
+    return {...object, ...props}
   }
 
   const head: Key = path[0]
 
   if (path.length === 1) {
-    // The base condition. You've finally traversed `object` by way of
-    // evaluating each element in `path`. Update the value at the
+    // The BASE CONDITION. You've fully traversed `object` by way of
+    // evaluating each element in `path`. Updates the value at the
     // destination.
     return {
       ...object,
-      [head]: isObject(state) ? {...object[head], ...state} : state,
+      [head]: isObject(props) ? {...object[head], ...props} : props,
     }
   }
 
-  // The recursive step. Spread the current properties and traverse
+  // The RECURSIVE STEP. Spread the current properties and traverse
   // into the head of `path`.
   return {
     ...object,
@@ -46,7 +48,7 @@ function update (object: PlainObject, path: Key[], state: any): PlainObject {
       ...update(
         object[head],
         path.slice(1),
-        state
+        props
       ),
     },
   }
