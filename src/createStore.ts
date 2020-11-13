e54fc7b56789df453686dc70dcd9130e27cd305c
David Yeung <quxbaz@gmail.com>
2020-11-13 01:04:07 -0700

    Filled out createStore.

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,19 +1,40 @@
-import {Store} from './types'
+import {Store, Listener} from './types'
 import {traverse, update} from './object-util'
 
 function createStore (initState: any): Store {
   let _state = initState
+  const _listeners: Listener[] = []
+
+  // Calls listeners.
+  function _publish (...args: Parameters<Listener>) {
+    _listeners.forEach(fn => fn(...args))
+  }
+
   return {
     // Gets the current store state.
     getState: () => _state,
 
-    // Merges a transform object into the store state. Returns the transformed
-    // slice of state respective of the `path` property in `transform`.
+    /*
+      Merges a transform object into the store state. Calls listeners. Returns
+      the transformed slice of state with respect to the `path` property in
+      `transform`.
+
+      ::TODO::
+      - Handle the case where transform.props is a function. evalTransform.
+      - Tests.
+    */
     commit: (transform) => {
-      const {name, path, props} = transform
-      _state = update(_state, path, props)
+      const {path, props} = transform
+      const prevState = _state
+      _state = update(_state, path, props)  // The next state.
+      _publish(prevState, _state, transform)
       return traverse(_state, path)
-    }
+    },
+
+    // ::TODO:: This should return an unsub function.
+    subscribe (listener) {
+      _listeners.push(listener)
+    },
   }
 }
 
