1bf5532b1d9376ad9237dc33c3269deac0a87acf
David Yeung <quxbaz@gmail.com>
2020-11-12 23:28:11 -0700

    Transform, Store types.

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,13 @@
 export type Key = string | number
 export type PlainObject = Record<Key, any>
+
+export interface Transform {
+  name?: string,
+  path: string[],
+  props: any,
+}
+
+export interface Store {
+  getState (): any,
+  commit (transform: Transform): any,
+}
