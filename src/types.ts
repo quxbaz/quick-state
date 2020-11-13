2f4030dcbbbc24dc6849e637a6fde395b1b00935
David Yeung <quxbaz@gmail.com>
2020-11-13 01:03:55 -0700

    Listener type.

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,10 @@ export interface Transform {
   props: any,
 }
 
+export type Listener = (prevState: any, nextState: any, transform: Transform) => any
+
 export interface Store {
   getState (): any,
   commit (transform: Transform): any,
+  subscribe (listener: Listener): void,
 }
