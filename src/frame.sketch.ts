be3da8eaa120832e13e3e943eb965979565c493e
David Yeung <quxbaz@gmail.com>
2020-11-14 23:57:24 -0700

    modified:   src/frame.sketch.ts

--- src/frame.sketch.ts
+++ src/frame.sketch.ts
@@ -1,11 +1,23 @@
-type State = object
-
-// interface Frame {
-//   state: State,
-//   events: Event[],
-// }
-
-type frame = (state: State, events: Event[]) => frame
-const frame: frame = (state, events) => {
-  return frame(state, events)
+const [event]Loop = function () {
+  while (isAppRunning) {
+    // Start frame
+    renderView()  // The view is always rendered, so this is unnecessary.
+    // ... Wait for events. How do we know when an event has arrived?
+    const event(s) = [...]  // Can we capture more than 1 event in a frame?
+    const transforms = events.map(e => mapEventToTransform(e))
+    const nextState = transforms.reduce((state, t) => applyTransform(state, t), state)
+    store.commit(nextState)
+    // Flush frame events
+    // End frame
+  }
 }
+
+queueEvent('click-button')
+queueEvent('click-button')
+queueEvent('click-button')
+
+
+/*
+  Isn't this just dispatch...? Except the work responsibility is moved from the
+  component callback to a central event loop.
+*/
