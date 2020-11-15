915c7febc02ee736d1b18a34a126dac3a1526963
David Yeung <quxbaz@gmail.com>
2020-11-15 00:11:15 -0700

    modified:   src/frame.sketch.ts

--- src/frame.sketch.ts
+++ src/frame.sketch.ts
@@ -2,8 +2,10 @@ const [event]Loop = function () {
   while (isAppRunning) {
     // Start frame
     renderView()  // The view is always rendered, so this is unnecessary.
+
     // ... Wait for events. How do we know when an event has arrived?
-    const event(s) = [...]  // Can we capture more than 1 event in a frame?
+    const events = [...]
+
     const transforms = events.map(e => mapEventToTransform(e))
     const nextState = transforms.reduce((state, t) => applyTransform(state, t), state)
     store.commit(nextState)
@@ -16,8 +18,17 @@ queueEvent('click-button')
 queueEvent('click-button')
 queueEvent('click-button')
 
-
 /*
   Isn't this just dispatch...? Except the work responsibility is moved from the
   component callback to a central event loop.
+*/
+
+/*
+  store.dispatch seems too powerful. dispatch is a mutative function even if it
+  doesn't appear so. It resolves to:
+
+    store.state = next state
+    store.trigger('update')
+
+  in a very direct way.
 */
