67261ad108a3254138c6ab751afe9422941f28dd
David Yeung <quxbaz@gmail.com>
2020-11-21 19:35:14 -0700

    Exports.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-export { default as getModuleName } from './getModuleName'
-export { default as createStore } from './createStore'
+import * as getModuleName from './getModuleName'
+import * as createStore from './createStore'
+
+// export { default as getModuleName } from './getModuleName'
+// export { default as createStore } from './createStore'
 
 // import getModuleName from './getModuleName'
 // import createStore from './createStore'
 
 // const __MODULE__ = 'quick-state'
+
+export {
+  getModuleName,
+  createStore,
+}
 
 // export {
 //   __MODULE__,
