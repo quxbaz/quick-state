39bb6252702b2c129c0c011d87fdbd9655bb6d08
David Yeung <quxbaz@gmail.com>
2020-11-22 15:52:41 -0700

    Fix module exports.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-import * as getModuleName from './getModuleName'
-import * as createStore from './createStore'
-
-// export { default as getModuleName } from './getModuleName'
-// export { default as createStore } from './createStore'
-
-// import getModuleName from './getModuleName'
-// import createStore from './createStore'
-
-// const __MODULE__ = 'quick-state'
+import createStore from './createStore'
 
 export {
-  getModuleName,
   createStore,
 }
-
-// export {
-//   __MODULE__,
-//   getModuleName,
-//   createStore,
-// }
